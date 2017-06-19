ca2511da4374b25202fc7efe74fce35e795cb029
yungshenglu <yungshenglu1994@gmail.com>
2017-06-19 15:09:37 +0800

    Modified the following issues. * Fix the redirection to sign-in and sign-up pages * Fix the disorder of the sign-in and sign-up buttons. * CONFUSE to fix the tab active status for the underscore.

--- client-service/js/account.js
+++ client-service/js/account.js
@@ -9,52 +9,55 @@ $(document).ready(function() {
     // tabs
     $('ul.tabs').tabs();
 
-    // Load start page animation
+    // Redirect start page to sign-in page
     $('.sct-start').delay(2000).fadeOut(function() {
         $(this).hide();
         $('.sct-account').show();
         $('#signin').show();
     });
 
-    $('#start-signup').click(function() {
+    // Redirect sigin-in page to sign-up id page
+    $('.rd-signup-id').click(function() {
         $('#signin').fadeOut(function() {
             $(this).hide();
             $('#signup-id').show();
         });
     });
 
-    $('#id-signin').click(function() {
+    // Redirect sign-up page to sign-in page
+    $('.rd-signin').click(function() {
+        if ($('#signup').is(':hidden')) {
+            $('#signup-id').fadeOut(function() {
+                $(this).hide();
+                $('#signin').show();
+            });
+        } else if ($('#signup-id').is(':hidden')) {
+            $('#signup').fadeOut(function() {
+                $(this).hide();
+                $('#signin').show();
+            });
+        }
+    });
+
+    // Redirect to passenger sign-up form
+    $('#rd-signup-pass').click(function() {
         $('#signup-id').fadeOut(function() {
             $(this).hide();
-            $('#signin').show();
+            $('#signup').show();
         });
+        // Add text information
+        $('#user-type-gp').val('passenger');
+        $('#user-type-fb').val('passenger');
     });
 
-
-    $('#start-signin').click(function() {
-        $('#signup').fadeOut(function() {
-            $(this).hide();
-            $('#signin').show();
-        });
-    });
-
-    $('#signup-pass').click(function() {
+    // Redirect to driver sign-up form
+    $('#rd-signup-driver').click(function() {
         $('#signup-id').fadeOut(function() {
             $(this).hide();
             $('#signup').show();
         });
         // Add text information 
-        $('#user-type-gp').val("passenger");
-        $('#user-type-fb').val("passenger");
-    });
-
-    $('#signup-driver').click(function() {
-        $('#signup-id').fadeOut(function() {
-            $(this).hide();
-            $('#signup').show();
-        });
-        // Add text information 
-        $('#user-type-gp').val("driver");
-        $('#user-type-fb').val("driver");
+        $('#user-type-gp').val('driver');
+        $('#user-type-fb').val('driver');
     });
 });
