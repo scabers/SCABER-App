9cfa3a7585416131e0390523ba4ce6b07c5295a2
yungshenglu <yungshenglu1994@gmail.com>
2017-06-19 21:42:06 +0800

    Fix the monitor button in navbar.

--- client-service/js/passenger.js
+++ client-service/js/passenger.js
@@ -2,28 +2,34 @@ $(document).ready(function() {
     // initialize passenger
     $('#pass-profile').hide();
 
-    // Bind passenger profile
-    $('.nav-profile').click(function() {
-        $('.pass-pages').hide();
-        $('#pass-profile').show();
-    });
-
     // Bind passenger riding
     $('.nav-riding').click(function() {
         $('.pass-pages').hide();
         $('#pass-riding').show();
     });
 
-    // Bind passenger message
-    $('.nav-message').click(function() {
+    // Bind passenger profile
+    $('.nav-profile').click(function() {
         $('.pass-pages').hide();
-        $('#pass-message').show();
+        $('#pass-profile').show();
+    });
+
+    // Bind passenger monitor
+    $('.nav-monitor').click(function() {
+        $('.pass-pages').hide();
+        $('#pass-monitor').show();
     });
 
     // Bind passenger rating
     $('.nav-rating').click(function() {
         $('.pass-pages').hide();
         $('#pass-rating').show();
+    });
+
+    // Bind passenger message
+    $('.nav-message').click(function() {
+        $('.pass-pages').hide();
+        $('#pass-message').show();
     });
 
     // Bind passenger setting
